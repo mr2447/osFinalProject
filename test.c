1afa0e56253848eb1b0f9ddb89ba1feeec7a5c78
Jesse Noppe-Brandon <162082874+jessenb16@users.noreply.github.com>
2024-12-01 12:39:16 -0500

    Create test.c

---
+++ test.c
@@ -0,0 +1,26 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+int main(void) {
+    //strace(1);  // Enable tracing
+
+    int i;
+    for (i = 0; i < 3; i++) {
+        int pid = fork();
+        if (pid == 0) {
+            //printf(1, "Child %d: Running\n", i);
+            char *args[] = {"echo", "hello", 0};
+            exec("echo", args);
+            exit();
+        }
+    }
+
+    for (i = 0; i < 3; i++) {
+        wait();
+    }
+
+    //strace_dump();  // Dump trace logs
+    //strace(0);      // Disable tracing
+    exit();
+}
