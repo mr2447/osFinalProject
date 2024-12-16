76733a6714d66d511f32413095c24aa8d784048f
Jesse Noppe-Brandon <162082874+jessenb16@users.noreply.github.com>
2024-12-15 23:40:28 -0500

    Update test.c

--- test.c
+++ test.c
@@ -3,8 +3,6 @@
 #include "user.h"
 
 int main(void) {
-    //strace(1);  // Enable tracing
-
     printf(1, "Hello, world!\n");
 
     int i;
@@ -23,45 +21,6 @@ int main(void) {
     }
 
     open("nonexistent_file.txt", 0); //shows a failed call
-
-    // strace_dump();  // Dump trace logs
-    //strace(0);      // Disable tracing
     exit();
 }
 
-
-
-// int main(void) {
-//     //strace(1);  // Enable tracing
-
-//     int i;
-//     for (i = 0; i < 3; i++) {
-//         int pid = fork();
-//         if (pid == 0) {
-//             int j;
-//             for (j = 0; j < 3; j++) {
-//                 int pid = fork();
-//                 if (pid == 0) {
-//                     //I want it to allocate memory and then exit
-//                     char *args[] = {"sbrk", "1000", 0};
-//                     exec("sbrk", args);
-//                     exit();
-           
-//                 }
-//             }
-//             for (j = 0; j < 3; j++) {
-//                 wait();
-//             }
-//         }
-//     }
-
-//     for (i = 0; i < 3; i++) {
-//         wait();
-//     }
-
-//     open("nonexistent_file.txt", 0);
-
-//     //strace_dump();  // Dump trace logs
-//     //strace(0);      // Disable tracing
-//     exit();
-// }
